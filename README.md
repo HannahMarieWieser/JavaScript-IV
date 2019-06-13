fc5289bac83f7b25eacd546d82c421a859da1575
HannahmarieWieser <nikauli@gmail.com>
2019-06-13 17:29:53 -0400

    MVP finished lambda classes

--- README.md
+++ README.md
@@ -67,15 +67,17 @@ const fred = new Instructor({
 });
 ```
 
-#### Person
 
+#### Person
+[x]
 * First we need a Person class. This will be our `base-class`
 * Person receives `name` `age` `location` all as props
 * Person receives `speak` as a method.
 * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
 
-#### Instructor
 
+#### Instructor
+[x]
 * Now that we have a Person as our base class, we'll build our Instructor class.
 * Instructor uses the same attributes that have been set up by Person
 * Instructor has the following unique props:
@@ -86,8 +88,9 @@ const fred = new Instructor({
   * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
   * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
 
-#### Student
 
+#### Student
+[x]
 * Now we need some students!
 * Student uses the same attributes that have been set up by Person
 * Student has the following unique props:
@@ -100,7 +103,7 @@ const fred = new Instructor({
   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
 
 #### Project Manager
-
+[x]
 * Now that we have instructors and students, we'd be nowhere without our PM's
 * ProjectManagers are extensions of Instructors
 * ProjectManagers have the following unique props:
@@ -113,7 +116,9 @@ const fred = new Instructor({
 #### Stretch Problem
 
 * Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
+
 * Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
+
 * Add a graduate method to a student.
   * This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
   * If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,155 @@
 // CODE here for your Lambda Classes
+
+
+//person class original
+
+class Person{
+    constructor(persFact){
+        this.name = persFact.name;
+        this.age = persFact.age;
+        this.location = persFact.location;
+    }   
+
+    speak(){
+        return  `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+
+//instructor class  extend-> of person
+
+class Instructor extends Person{
+    constructor(instFacts){
+        super(instFacts);
+        this.specialty = instFacts.specialty;
+        this.favLanguage = instFacts.favLanguage;
+        this.catchPhrase = instFacts.catchPhrase;
+    }
+
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    }
+
+    grade(name, subject){
+        return `${name} receives a perfect score on ${subject}`
+    }
+}
+
+
+//student class extend-> of person
+class Student extends Person{
+    constructor(studFacts){
+        super(studFacts)
+        this.previousBackground = studFacts.previousBackground;
+        this.className = studFacts.className;
+        this.favSubjects = studFacts.favSubjects;
+    }
+
+    listsSubjects(){
+        return this.favSubjects;
+    }
+
+    PRAssignment(subject){
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+
+    sprintChallenge(subject){
+        return `${Student.name} has begun sprint challenge on ${subject}`;
+    }
+}
+
+
+
+//project manager  extend-> of instrcutor
+
+class ProjectManager extends Instructor{
+    constructor(PMfacts){
+        super(PMfacts)
+        this.gradClassName = PMfacts.gradClassName;
+        this.favInstructor = PMfacts.favInstructor;
+    }
+
+    standup(name, slack){
+        return `${name} announces to ${slack}, @channel standy times!​​​​​`;
+    }
+
+    debugsCode(Student, subject){
+        return `${this.name} debugs ${Student.name}'s code on ${subject}`;
+    }
+}
+
+
+///test objects
+//person
+const Mary = new Person({
+    name : 'Mary',
+    age : 22,
+    location: 'Michigan'
+});
+
+const LemonSnicket = new Person({
+    name : 'Lemony Snicket',
+    age : 60,
+    location : 'Britain'
+});
+
+
+//instructor
+
+const Julio = new Instructor({
+    specialty : 'Dark Arts',
+    favLanguage : 'C++',
+    catchPhrase : 'Enjoy your torture',
+})
+
+const Erin = new Instructor({
+    specialty : 'Front End',
+    favLanguage : 'CSS',
+    catchPhrase : 'Look how clean this is!',
+})
+
+
+
+
+//student
+const billy = new Student({
+    name : 'billy',
+    previousBackground : 'Architecture',
+    className : 'CS404',
+    favSubjects : ['Html', 'CSS']
+})
+
+const alexa = new Student({
+    name : 'alexa',
+    previousBackground : 'Chef',
+    className : 'CS1201',
+    favSubjects : ['C#', 'Javascript', 'Python']
+})
+
+
+//Project Manager
+
+const BrandonSandon = new ProjectManager({
+    name: 'BrandonSandon',
+    location : 'FantasyLand',
+    gradClassName : 'CS0',
+    favInstructor : 'Kelsier'
+})
+
+const Spiderman = new ProjectManager({
+    name : 'Spiderman',
+    gradClassName : 'CS12',
+    favInstructor : 'DocOc'
+})
+
+
+
+
+console.log(Mary.speak());
+console.log(BrandonSandon.speak());
+console.log(BrandonSandon.demo('Pits of Hathsin'))
+console.log(Julio.grade('Amy', 'Basketball'));
+console.log(billy.listsSubjects());
+console.log(alexa.PRAssignment('Music'));
+console.log(BrandonSandon.standup('BrandonSandon', 'MainChannel'));
+console.log(Spiderman.debugsCode(billy,'art'));
